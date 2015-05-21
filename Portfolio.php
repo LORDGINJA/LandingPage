952cbe4bc1c0e6adb370d94b6dbc7c6d5a725579
Maxwell Karp <maxwell.karp@yahoo.com>
2015-05-20 23:01:10 -0700

    Starting Landing page

---
+++ Portfolio.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<link rel="stylesheet" type="text/css" href="css/port.css">
+		<title>Landing Page</title>
+	</head>
+	<header>
+  <nav>
+
+  <ul>
+
+    <li>
+      <a href="#" class="logo">
+        <img src="https://avatars3.githubusercontent.com/u/1972124?v=2&s=460">
+      </a>
+    </li>
+    <li><h1>Portfolio</h1></li>
+  </ul>
+</nav>
+</header>
+	<body>
+		<div>
+			<p>
+				My first project this year was to create a portfolio, which is basically the less professional version of a alnding page.
+				This project used HTML and CSS. To view my porfolio, <a href="../ME/index.html">click here.</a>
+			</p>
+		</div>
+	</body>
+</html>
---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link type="text/css" rel="stylesheet"  href="css/bootstrap.css">
+	<link type="text/css" rel="stylesheet"  href="css/bootstrap-theme.css">
+	<link type="text/css" rel="stylesheet"  href="css/bootstrap-theme.css.map">
+	<link type="text/css" rel="stylesheet"  href="css/bootstrap.css.map">
+	<link type="text/css" rel="stylesheet"  href="css/main.css">
+	<title></title>
+</head>
+<body>
+	 <nav role='navigation'>
+	    <ul>
+	      <li><a href="#home">Home</a></li>
+	      <li><a href="#about">About</a></li>
+	      <li><a href="#clients">Clients</a></li>
+	      <li><a href="#portfolio">Portfolio</a></li>
+	      <li><a href="#are">Who We Are</a></li>
+	      <li><a href="#do">What We Do</a></li>
+	      <li><a href="#contact">Contact Us</a></li>
+	    </ul>
+	  </nav>
+
+  <!-- hamburger icon -->
+  <i class="fa fa-reorder"></i>
+</body>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+</html>
